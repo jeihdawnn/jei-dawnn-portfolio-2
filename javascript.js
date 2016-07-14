eeab0736a055068d192b4a6af6e32bf1ea4721a9
Jei Dawnn <khachieu@outlook.com>
2016-07-15 00:09:13 +0700

    added Animations to About Section and Scroll Icon

--- javascript.js
+++ javascript.js
@@ -1,19 +1,49 @@
+        
+var myVar;
+function myFunction() {
+    myVar = setTimeout(showPage, 2000);
+};   
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myPage").style.display = "block";
+};
+
+$(document).ready(function(){
+//Navigation
 function w3_open() {     
     document.getElementById("mySidenav").style.display = "block";
     document.getElementById("opennav").style.visibility = "hidden";
     };
-
 function w3_close() {
     document.getElementById("mySidenav").style.display= "none";
     document.getElementById("opennav").style.visibility = "visible";
     };
-        
-var myVar;
-function myFunction() {
-    myVar = setTimeout(showPage, 3000);
-    };   
 
-function showPage() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("myPage").style.display = "block";
+//Scrolling
+$(window).scroll(function(){
+    var wScroll = $(document).scrollTop();
+    if (wScroll > $('.about-bg').offset().top - ($(window).height() / 1.2)) {
+        $(".left").each(function(i){
+            setTimeout(function(){
+            $(".left").eq(i).addClass("animated-left show");}, (700 * (Math.exp(i * 0.14))) - 700); 
+        });
+        $(".right").each(function(i){
+            setTimeout(function(){
+            $(".right").eq(i).addClass("animated-right show");}, (700 * (Math.exp(i * 0.14))) - 700); 
+        });
+        $(".about-bg img").addClass("animated-scale");
     };
+    
+    if (wScroll > $('.about').offset().top - ($(window).height() / 1.2)) {
+        $("#aboutme").fadeIn(400);
+    };
+    if (wScroll > 300 && wScroll < 600) {
+        $(".scroll").fadeOut(700);
+    } else {
+        $(".scroll").fadeIn(700);
+    };
+    
+    
+});
+
+});
