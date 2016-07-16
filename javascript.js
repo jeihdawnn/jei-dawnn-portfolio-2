2f8f4d1e138c90f220c37a55a191b20969093446
Jei Dawnn <khachieu@outlook.com>
2016-07-16 10:04:56 +0700

    deleted draft works, add Animations to Contact

--- javascript.js
+++ javascript.js
@@ -1,12 +1,22 @@
-        
 var myVar;
 function myFunction() {
     myVar = setTimeout(showPage, 2500);
 };   
-function showPage() {
+function showPage() {   
     document.getElementById("loader").style.display = "none"; document.getElementById("myPage").style.display = "block";
 };
 
+
+
+function myFun(){
+        $(".side-navigation").fadeToggle();
+        $(".bar1").toggleClass("invisible");
+        $(".bar3").toggleClass("invisible");
+};
+
+if ($(window).height() >= 1020) {
+        $('.side-navigation').css('display','none');
+    };
 $(document).ready(function(){
 //Navigation
     
@@ -38,8 +48,17 @@ $(window).scroll(function(){
     
     if (wScroll > $('.description').offset().top - ($(window).height() / 1)) {
         $(".character").fadeIn(2000);
-    }
+    };
     
+    if (wScroll > $('.available').offset().top - ($(window).height() / 1.2)) {
+        $('.available').addClass('w3-animate-left');
+    };
+    if (wScroll > $('.extra-text').offset().top - ($(window).height() / 1.2)) {
+        $('.extra-text').addClass('w3-animate-right');
+    };
+    if (wScroll > $('#end-logo').offset().top - ($(window).height() / 1.2)) {
+        $('#end-logo').addClass('w3-animate-bottom');
+    };
 });
 //end of Scrolling Function
 });
